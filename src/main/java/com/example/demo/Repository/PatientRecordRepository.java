11e1536dae0670199d2f12babb7e406ec6e02ab4
20IT118 <80442907+drabadia007@users.noreply.github.com>
2022-08-25 12:28:00 +0530

    Delete PatientRecordRepository.java

--- src/main/java/com/example/demo/Repository/PatientRecordRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.PatientRecord;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PatientRecordRepository extends JpaRepository<PatientRecord, Integer> {
-}
